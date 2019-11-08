141d213da5ff8a865c8587b125bd285c31bc244d
Will Sutter <kingsutter12@gmail.com>
2019-11-08 10:54:51 -0600

    set up favorites table

--- database.sql
+++ database.sql
@@ -12,3 +12,16 @@ CREATE TABLE "category" (
 -- Default categories. You may change them :)
 INSERT INTO "category" ("name")
 VALUES ('funny'), ('vega'), ('cartoon'), ('nsfw'), ('meme');
+
+--------------------------------------
+-- Favorites table
+CREATE TABLE "favorites" (
+    "id" SERIAL PRIMARY KEY,
+    "title" VARCHAR (280) NOT NULL,
+    "url" VARCHAR (2083) NOT NULL,
+    "catagory_id" INT REFERENCES "catagory"
+);
+
+-- Default favorites. You may change them :)
+INSERT INTO "favorites" ("name","url","catagory_id")
+VALUES ('mr. meseeks','https://media.giphy.com/media/lpxqmxzQHL7hUejepu/giphy.gif','3'), ('nyan cat','http://giphygifs.s3.amazonaws.com/media/sIIhZliB2McAo/giphy.gif','5'), ('mac ISAIP','https://media.giphy.com/media/MEpGhvzZjtaVf1V0v8/giphy.gif','1');
