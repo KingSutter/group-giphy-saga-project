95739fe2993f8491a72df665104c5937f4d5265d
Will Sutter <kingsutter12@gmail.com>
2019-11-08 11:41:52 -0600

    got Favorites list working

--- src/components/FavoritesList/FavoritesList.js
+++ src/components/FavoritesList/FavoritesList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { GridList } from '@material-ui/core';
+import { GridList,GridListTile } from '@material-ui/core';
+import FavoritesItem from '../FavoritesItem/FavoritesItem';
 
 const mapStateToProps = reduxState => ({
     reduxState,
@@ -8,27 +9,31 @@ const mapStateToProps = reduxState => ({
 
 class FavoritesList extends Component {
     componentDidMount() {
-        // use component did mount to dispatch an action to request the plantList from the API
-        this.props.dispatch({type: 'GET_FAVORITES'})
-    }
-
-    render() {
-        return (
-            <GridList
-            cellHeight={160} className={classes.gridList} cols={3}
-            >
-                {this.props.reduxState.favoritesList.map((image) => (
-                    <GridListTile
-                    key={image.id}
-                    item
-                    sm={3}
-                    >
-                       <FavoritesItem image={image}/>
-                    </GridListTile>
-                ))}
-            </GridList>
-        );
-    }
-}
+      // use component did mount to dispatch an action to request the plantList from the API
+      this.props.dispatch({type: 'GET_FAVORITES'})
+  }
+  
+  render() {
+      return (
+          <>
+          <p>hi</p>
+          <GridList
+          cellHeight={160} cols={4}
+          >
+              {this.props.reduxState.favoritesReducer.map((image) => (
+                  <GridListTile
+                  key={image.id}
+                  item
+                  sm={3}
+                  >
+                     <FavoritesItem image={image}/>
+                  </GridListTile>
+              ))}
+          </GridList>
+              {/* <pre>{JSON.stringify(this.props,null,2)}</pre> */}
+          </>
+      );
+  }
+  }
 
 export default connect(mapStateToProps)(FavoritesList);
