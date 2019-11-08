5bf6f94287dda55d7dec42a554a89a7d03b9a965
Luke Schleder <luke.schleder@gmail.com>
2019-11-08 14:47:03 -0600

    create css and material ui button

--- src/components/SearchView/SearchView.js
+++ src/components/SearchView/SearchView.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { GridList,GridListTile } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 import SearchItem from '../SearchItem/SearchItem';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 class App extends Component {
     state = {
@@ -30,14 +32,9 @@ class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
-      <div>
-        <header className="App-header">
-          <h1>Search Giphy API</h1>
-        </header>
-        <Link to="/FavoritesList">Favorites</Link>
-        
-        <input value={this.state.newSearch} onChange={this.handleChange} />
-        <button onClick={this.searchGiphy}>Search GIPHY</button>
+      <div>    
+        <TextField value={this.state.newSearch} onChange={this.handleChange} />
+        <Button variant="contained" onClick={this.searchGiphy}>Search GIPHY</Button>
 
         <GridList
            cols={3}
